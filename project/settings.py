5416011a9e612d3b553d7ec01a676c84a43eda12
timashkov <timashkov060896@gmail.com>
2024-08-11 10:49:52 +0300

    добавил добро на хотс хостинга

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-^91jqoqx*^#48tqa2q4#+uf(41+b07md%%tkjfrxp_t$dg(z@e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'todo-list-backend-tbwi.onrender.com'
+]
 
 
 # Application definition
